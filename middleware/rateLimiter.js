3d71f57a446d819344353b3aa2cf3564862bb9bc
zechsoft <zechsoft.it@gmail.com>
2026-02-05 13:50:46 +0530

    ipih

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -43,7 +43,7 @@ const authLimiter = rateLimit({
 const speedLimiter = slowDown({
     windowMs: 15 * 60 * 1000,
     delayAfter: 50,
-    delayMs: 500
+    delayMs: () => 500
 });
 
 module.exports = {
